cc3726078e74a6b0ebc8e665643c374a9496a5c4
Crowley <handhfa45@gmail.com>
2023-05-05 19:35:56 +0300

    add commit

---
+++ src/components/Drawer/index.js
@@ -0,0 +1,103 @@
+import React from "react";
+import axios from "axios";
+
+import Info from "../Info";
+import { useCart } from "../../hooks/useCart";
+
+import styles from "./Drawer.module.css";
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function Drawer({onClose, onRemove, items = [], opened }) {
+  const { cartItems, setCartItems, totalPrice} = useCart();
+  const [orderId, setOrderId] = React.useState(null);
+  const [isOrderComplete, setIsOrderComplite] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const onClickOrder = async () => {
+    try {
+      setIsLoading(true);
+      const {data} = await axios.post("https://63dfd8b259bccf35dabacd86.mockapi.io/orders", {
+        items: cartItems,
+      });
+      setOrderId(data.id)
+      setIsOrderComplite(true);
+      setCartItems([]);
+
+      for(let i = 0; i < cartItems.length; i++) {
+        const item = cartItems[i];
+        await axios.delete("https://63d81aa75dbd72324433552c.mockapi.io/Cart/" + item.id);
+        await delay(1000);
+      }
+
+    } catch (error) {
+      alert('Ошибка при создании заказа :)')
+    }
+    setIsLoading(false)
+  };
+
+  return (
+    <div className={`${styles.overlay} ${opened ? styles.overlayVisible : ''}`}>
+      <div className = {styles.drawer}>
+        <h2 className="d-flex justify-between mb-30">
+          Кошик
+          <img
+            onClick={onClose}
+            className="cu-p"
+            src="img/btn-remove.svg"
+            alt="Close"
+          />
+        </h2>
+
+        {items.length > 0 ? (
+          <div className="d-flex flex-column flex">
+            <div className="items flex">
+              {items.map((obj) => (
+                <div key={obj.id} className="cartItem d-flex align-center mb-20">
+                  <div
+                    style={{ backgroundImage: `url(${obj.imageUrl})` }}
+                    className="cartItemImg"
+                  ></div>
+                  <div className="mr-20 flex">
+                    <p className="mb-5">{obj.title}</p>
+                    <b>{obj.price} грн.</b>
+                  </div>
+                  <img
+                    onClick={() => onRemove(obj.id)}
+                    className="removeBtn"
+                    src="img/btn-remove.svg"
+                    alt="btn-remove"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="cartTotalBlock">
+              <ul>
+                <li>
+                  <span>Разом:</span>
+                  <div></div>
+                  <b>{totalPrice} руб.</b>
+                </li>
+                <li>
+                  <span>Налог 5%:</span>
+                  <div></div>
+                  <b>{totalPrice * 0.05} грн.</b>
+                </li>
+              </ul>
+              <button disabled={isLoading} onClick={onClickOrder} className="greenButton">
+                Оформити замовлення <img src="/img/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+          </div>
+        ) : (
+          <Info 
+          title={isOrderComplete ? "Замовлення оформлено" : "Кошик Порожній" }
+          description={isOrderComplete ? `Ваше замовлення #${orderId} скоро буде передано кур'єрській доставці.` : "Додати хоча б одну пару кросівок, щоб зробити замовлення." }
+          image={isOrderComplete ? "img/complete-order.jpg" : "img/empty-cart.jpg"}/>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Drawer;
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React from "react";
+import Card from "../components/Card";
+
+function Orders() {
+  const [orders, setOrders] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(
+          "https://6422e958001cb9fc2033295b.mockapi.io/orders"
+        );
+        setOrders(data.reduce((prev, obj) => [...prev, ...obj.items], []));
+        setIsLoading(false);
+      } catch (error) {
+        alert("Ошибка при запросе заказов");
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  return (
+    <div className="content p-40">
+      <div className="d-flex align-center justify-between mb-40">
+        <h1>Мої замовлення</h1>
+      </div>
+
+      <div className="d-flex flex-wrap">
+        {(isLoading ? [...Array(8)] : orders).map((item, index) => (
+          <Card
+            key={index}
+            loading={isLoading}
+            {...item}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Orders;
